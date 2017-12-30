7a2e53bd185d990960202fd7fba4d9a390e6c757
Paweł Gorzelany <pawel.gorzelany@tooploox.com>
2017-12-30 23:55:29 +0100

    day seven part two, also added stack.yaml to use lts-10.1

--- day7.hs
+++ day7.hs
@@ -1,28 +1,64 @@
 {-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
 
 import Control.Monad
+import Data.Ord
+import Data.List
+import Data.Tree
+import qualified Data.Map.Strict as Map
+
+type Name = String
+type Weight = Int
+type Program = (Name, Weight)
+type Children = [Name]
 
 
-strip :: Char -> String -> String
+strip :: (Eq a) => a -> [a] -> [a]
 strip x xs = filter (/= x) xs
 
 
-parse :: [String] -> (String, [String])
-parse (x:xs) = (x, map (strip ',') $ drop 2 xs)
+weight :: String -> Int
+weight = read . strip '(' . strip ')'
 
 
-hasChildren :: (String, [String]) -> Bool
-hasChildren (_, []) = False
+parse :: [String] -> (Name, (Program, Children))
+parse (x:w:[]) = (x, ((x, weight w), []))
+parse (x:w:"->":xs) = (x, ((x, weight w), strip ',' <$> xs))
+
+
+hasChildren :: (Name, (Program, Children)) -> Bool
+hasChildren (_, (_, [])) = False
 hasChildren _ = True
 
 
-root :: [(String, [String])] -> String
+root :: [(Name, (Program, Children))] -> Name
 root programs = head $ filter (not . flip elem children) candidates
-    where candidates = map fst $ filter hasChildren programs
-          children = concatMap snd programs
+    where candidates = fst <$> filter hasChildren programs
+          children = concatMap (snd . snd) programs
+
+
+addWeights :: Program -> [Weight] -> Weight
+addWeights p xs = snd p + sum xs
+
+
+balance :: Weight -> Tree Program -> Weight
+balance diff (Node (_, w) children) =
+    if balanced then w + diff else balance diff' tree'
+        where balanced = (==) 1 $ length $ nub folds
+              folds = foldTree addWeights <$> children
+              diff' = minimum folds - maximum folds
+              tree' = case index of
+                  Just i -> children !! i
+              different = head . minimumBy (comparing length) . group
+              index = elemIndex (different folds) folds
+
+
+tree :: [(Name, (Program, Children))] -> Tree Program
+tree programs = unfoldTree (\k -> m Map.! k) $ root programs
+    where m = Map.fromList programs
 
 
 main :: IO ()
 main = do
   puzzle <- (map parse . map words . lines) `liftM` getContents
   putStrLn $ "Part one " ++ root puzzle
+  putStrLn $ "Part two " ++ (show . balance 0 $ tree puzzle)
